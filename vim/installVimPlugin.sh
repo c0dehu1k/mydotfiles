f6a1691abe4b6201dd12b2692fbe5e93ffbd1c67
Sunil Joshi <sunil.joshi@knowlarity.com>
2016-07-11 23:51:39 +0530

    Shell script to install very best vim plugins

---
+++ vim/installVimPlugin.sh
@@ -0,0 +1,15 @@
+mkdir -p ~/.vim/bundle
+cd ~/.vim/bundle
+git clone git@github.com:jistr/vim-nerdtree-tabs.git
+git clone git@github.com:tpope/vim-sensible.git
+git clone git@github.com:ctrlpvim/ctrlp.vim.git
+git clone git@github.com:tpope/vim-fugitive.git
+git clone git@github.com:tpope/vim-surround.git
+git clone git@github.com:klen/python-mode.git
+git clone git@github.com:scrooloose/nerdtree.git
+git clone git@github.com:Valloric/YouCompleteMe.git
+git clone git@github.com:scrooloose/nerdcommenter.git
+git clone git@github.com:vim-airline/vim-airline.git
+git clone git@github.com:vim-airline/vim-airline-themes.git
+git clone git@github.com:scrooloose/syntastic.git
+git clone git@github.com:scrooloose/nerdtree.git
