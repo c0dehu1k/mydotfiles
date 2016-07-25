d97d73f351323beebeab8044f2e44ca4b6edd69e
Sunil Joshi <sunil.joshi@knowlarity.com>
2016-07-25 23:01:05 +0530

    Recurisive script for YCM

--- vim/installVimPlugin.sh
+++ vim/installVimPlugin.sh
@@ -8,6 +8,9 @@ git clone git@github.com:tpope/vim-surround.git
 git clone git@github.com:klen/python-mode.git
 git clone git@github.com:scrooloose/nerdtree.git
 git clone git@github.com:Valloric/YouCompleteMe.git
+cd ~/.vim/bundle/YouCompleteMe
+git submodule update --init --recursive
+cd ..
 git clone git@github.com:scrooloose/nerdcommenter.git
 git clone git@github.com:vim-airline/vim-airline.git
 git clone git@github.com:vim-airline/vim-airline-themes.git
