c1bf400e6dcffdff947ad8b238ef18853339dc8a
Sunil Joshi <joshi.sunil@outlook.com>
2017-04-14 13:24:11 +0530

    Added vim-yaml
    Yaml git link addded

--- vim/installVimPlugin.sh
+++ vim/installVimPlugin.sh
@@ -1,5 +1,6 @@
 mkdir -p ~/.vim/bundle
 cd ~/.vim/bundle
+git clone https://github.com/avakhov/vim-yaml
 git clone git@github.com:jistr/vim-nerdtree-tabs.git
 git clone git@github.com:tpope/vim-sensible.git
 git clone git@github.com:ctrlpvim/ctrlp.vim.git
