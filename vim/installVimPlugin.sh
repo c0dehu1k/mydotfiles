be6fba62e52fc6f90732fa21937afb74d21456de
Sunil Joshi <joshi.sunil@outlook.com>
2017-11-23 15:37:56 +0530

    Python-mode relying on 3rd party modules

--- vim/installVimPlugin.sh
+++ vim/installVimPlugin.sh
@@ -10,7 +10,6 @@ git clone git@github.com:tpope/vim-sensible.git
 git clone git@github.com:ctrlpvim/ctrlp.vim.git
 git clone git@github.com:tpope/vim-fugitive.git
 git clone git@github.com:tpope/vim-surround.git
-git clone git@github.com:klen/python-mode.git
 git clone git@github.com:scrooloose/nerdtree.git
 git clone git@github.com:scrooloose/nerdcommenter.git
 git clone git@github.com:vim-airline/vim-airline.git
@@ -22,7 +21,10 @@ git clone git@github.com:Valloric/YouCompleteMe.git
 cd ~/.vim/bundle/YouCompleteMe
 git submodule update --init --recursive
 cd ..
-
+git clone git@github.com:klen/python-mode.git
+cd ~/.vim/bundle/python-mode
+git submodule update --init --recursive
+cd ..
 
 # All Famous ColorSchemes for vim
 mkdir -p ~/.vim/colors
